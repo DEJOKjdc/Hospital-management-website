8eb94d767d8126decf4dd48de26c0db310b5948c
SHREEVISHNU <141863038+DEJOKjdc@users.noreply.github.com>
2025-03-25 10:53:05 +0530

    Update vite.config.ts

--- server/vite.config.ts
+++ server/vite.config.ts
@@ -1,30 +1,13 @@
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path from "path";
 
 export default defineConfig({
-  plugins: [react(), themePlugin()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-   outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  plugins: [react()],
   server: {
-    host: "0.0.0.0",
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true
-      }
-    }
-  }
+    strictPort: true,
+  },
 });
+
+
+
